4c34210c0c79507f2d4d480f40b6f1368c7d17b7
mr_zhai_sp@126.com <mr_zhai_sp@126.com>
2018-08-26 12:30:08 +0800

    five xiugai

--- Bussniss/Bussniss.py
+++ Bussniss/Bussniss.py
@@ -343,7 +343,8 @@ class Bussniss():
         self.p.dissMiss()
         u"""第二次修改"""
         u"""第三次修改"""
-        u"""第四次修改"""u"""第四次修改"""u"""第四次修改"""u"""第四次修改"""u"""第四次修改"""
+        u"""第五次修改"""
+
 sh = Bussniss()
 sh.buLuShuxing("bazhiwei","Cmcc@121122")
 
